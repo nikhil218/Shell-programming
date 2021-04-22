0c0234d2fc15924a836d8f4af68533cbd19833d3
nikhil218 <35371458+nikhil218@users.noreply.github.com>
2021-04-23 03:42:09 +0530

    Update tempConversion.sh

--- functions/tempConversion.sh
+++ functions/tempConversion.sh
@@ -8,15 +8,11 @@ function TempConversion(){
 	case $a in
 		0)
 			read -p "Enter a number to convert to Fahrenheit.The number should be between 0C and 100C : " degC
-			#degF=$(($(($degC*9/5))+32))
-			#echo $degF
 			echo - | awk '{printf "%.2f\n",'$(($degC*9/5))+32'}'
 			;;
 
 		1)
 			read -p "Enter a number to convert to Celsius. The number should be between 32F and 212F : " degF
-			#degC=$(( $(( $degF-32 )) * 5/9 ))
-			#echo $degC
 			echo - | awk '{printf "%.2f\n",'$(($degF-32))*5/9'}'
 			;;
 	esac
